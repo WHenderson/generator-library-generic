128c227c32e38b19dc8de5efb7d167dfc0ab6665
William Henderson/AUS/CSC <whenderson7@csc.com>
2016-04-26 12:14:34 +1000

    Basics

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,1 +1,10 @@
+var generators = require('yeoman-generator');
 
+module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+    },
+    dummyMethod: function () {
+        console.log('dummyMethod');
+    }
+});
