d2fa6ba7538e704eec39a073297707ac04c7af52
William Henderson/AUS/CSC <whenderson7@csc.com>
2016-04-26 14:06:12 +1000

    Adding npm-init and license

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,50 @@
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
     constructor: function () {
         generators.Base.apply(this, arguments);
+
     },
-    dummyMethod: function () {
-        console.log('dummyMethod');
+    initializing: function () {
+        this.composeWith('npm-init', {
+            options: {
+                'skip-test': true,
+                'skip-license': true,
+                'version': '0.0.0',
+                'main': 'dist/index.js',
+                'license': 'Apache-2.0'
+            }
+        }, {
+            local: require.resolve('generator-npm-init')
+        });
+
+        this.composeWith('license', {
+
+        }, {
+            local: require.resolve('generator-license')
+        });
+    },
+    writingPackaging: function () {
+        const pkg = this.fs.readJSON(this.destinationPath('package.json'));
+
+        _.merge(pkg, {
+            'files': [
+                'dist'
+            ],
+            'scripts': {
+                'preversion': 'npm-git-library-version preversion',
+                'version': 'npm-git-library-version version',
+                'postversion': 'npm-git-library-version postversion',
+                'build': 'gulp chained-1-build',
+                'test': 'gulp',
+                'coveralls': 'coveralls --verbose < coverage/lcov.info'
+            },
+            'main': 'dist/index.js'
+        });
+
+        this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+
+        this.fs.write(this.destinationPath('src/index.js'), '');
     }
 });
